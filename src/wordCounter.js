95fc60c4bf51add0fde0a719e31e0c106f758e03
MarySalemme <marysalemme1@gmail.com>
2017-10-03 17:22:53 +0100

    Implementing function to define the processed array

--- src/wordCounter.js
+++ src/wordCounter.js
@@ -1,5 +1,12 @@
-function WordCounter() {
+var FileSystemUtils = require('./fileSystemUtils');
 
+function WordCounter() {
+  this.processedArray = [];
+  this.fsu = new FileSystemUtils();
 }
+
+WordCounter.prototype.setProcessedArray = function(fileName) {
+  this.processedArray = this.fsu.readFileToArray(fileName);
+};
 
 module.exports = WordCounter;
--- test/wordCounter.js
+++ test/wordCounter.js
@@ -1,7 +1,25 @@
 var chai = require('chai');
 var expect = chai.expect;
 var WordCounter = require('../src/wordCounter');
+var FileSystemUtils = require('../src/fileSystemUtils');
 
 describe('WordCounter', function() {
   wordCounter = new WordCounter();
+
+  it('is initialised with an empty array', function() {
+    expect(wordCounter.processedArray).to.be.empty;
+  });
+
+  it('is initialised with an instance of FileSystemUtils', function() {
+    expect(wordCounter.fsu).to.be.instanceOf(FileSystemUtils);
+  });
+
+
+  describe('#setProcessedArray', function() {
+    it('adds the processed file to the array', function() {
+      wordCounter.setProcessedArray('testFile');
+      expect(wordCounter.processedArray).to.deep.equal([ 'this', 'is', 'just', 'for', 'testing', 'purposes' ])
+    });
+  });
+
 });
